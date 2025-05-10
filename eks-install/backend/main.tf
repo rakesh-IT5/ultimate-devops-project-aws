51041b392eb8dec750b0d1d94f8ef0194218f3a7
rakesh-IT5 <166203828+rakesh-IT5@users.noreply.github.com>
2025-05-10 09:30:14 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "rakesh-terraform-eks-state-s3-bucket12"
+  bucket = "rakesh-terraform-eks-state-s3-bucket123"
 
   lifecycle {
     prevent_destroy = false
